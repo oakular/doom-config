c8c36a8455003a33d418268ff829cb075f4203e6
callumw <callum@codemakers.co.uk>
2021-02-09 21:44:41 +0000

    Add vagrant provision keybinding

--- modules/tools/vagrant/config.el
+++ modules/tools/vagrant/config.el
@@ -4,6 +4,11 @@
   :config
   (map! :leader
         (:prefix "v" :desc "Vagrant Up"  :n "u" #'vagrant-up)
-        (:prefix "v" :desc "Vagrant SSH" :n "s" #'vagrant-ssh)
+        (:prefix "v" :desc "Vagrant SSH" :n "s" #'vagrant-tramp-term)
         (:prefix "v" :desc "Vagrant Suspend" :n "d" #'vagrant-suspend)
-        (:prefix "v" :desc "Vagrant Destroy" :n "D" #'vagrant-destroy)))
+        (:prefix "v" :desc "Vagrant Destroy" :n "D" #'vagrant-destroy)
+        (:prefix "v" :desc "Vagrant Halt" :n "h" #'vagrant-halt)
+        (:prefix "v" :desc "Vagrant Provision" :n "p" #'vagrant-provision)))
+
+(after! vagrant
+  (set-popup-rule! "^\\*Vagrant\\*" :side 'bottom :quit 'other))
