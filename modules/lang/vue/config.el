f919cc2ece209ddbe5f27d3cfafff7fb213aa1ad
callumw <callum@codemakers.co.uk>
2021-02-09 21:44:29 +0000

    Use web-mode for Vue

--- modules/lang/vue/config.el
+++ modules/lang/vue/config.el
@@ -1,6 +1,11 @@
 ;;; editor/vue/config.el -*- lexical-binding: t; -*-
 
-(add-to-list 'auto-mode-alist '("\\.vue\\'" . vue-mode))
-(add-hook 'mmm-mode-hook
-          (lambda ()
-            (set-face-background 'mmm-default-submode-face nil)))
+;; (add-to-list 'auto-mode-alist '("\\.vue\\'" . vue-mode))
+;; (add-hook 'mmm-mode-hook
+;;           (lambda ()
+;;             (set-face-background 'mmm-default-submode-face nil)))
+;; (add-hook 'vue-mode-hook #'lsp!)
+
+(define-derived-mode vue-web-mode web-mode "Vue Web Mode")
+(add-to-list 'auto-mode-alist '("\\.vue\\'" . vue-web-mode))
+(add-hook 'vue-web-mode-hook #'lsp!)
---
+++ modules/lang/vue/packages.el
@@ -0,0 +1,4 @@
+;; -*- no-byte-compile: t; -*-
+;;; lang/vue/packages.el
+
+(package! polymode)
