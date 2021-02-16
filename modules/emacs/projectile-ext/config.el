cbae3c731408266c700712dc4e66a0dcd70f79cf
callumw <callum@codemakers.co.uk>
2021-02-16 17:02:39 +0000

    Don't cache vendor dirs in projectile

--- modules/emacs/projectile-ext/config.el
+++ modules/emacs/projectile-ext/config.el
@@ -3,6 +3,7 @@
 (after! projectile
   (setq projectile-switch-project-action 'projectile-dired)
   (setq counsel-projectile-switch-project-action 'counsel-projectile-switch-project-action-dired)
+  (add-to-list 'doom-projectile-cache-blacklist '".*vendor/")
   (map! :leader
         :map projectile-mode-map
         (:prefix "d" "f" #'projectile-find-dir)))
